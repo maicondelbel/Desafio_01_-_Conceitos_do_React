e0c93d3f3d896b0eeb634532ffb6bccc93d5fd29
Maicon P. Del Bel <maicon.delbel@gmail.com>
2023-02-01 13:56:22 -0300

    Challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,49 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) {
+      return
+    }
+
+    const newTask:Task = {      
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks((prevState) => {
+      return [
+        ...prevState,
+        newTask
+      ]
+    })
+
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksChanged = tasks.map((task) => {
+      if (task.id === id) {
+        return {
+          ...task,
+          isComplete: !task.isComplete
+        }
+      }
+      return task
+    })
+
+    setTasks(tasksChanged)
+
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const tasksFiltered = tasks.filter((task) => {
+      return task.id !== id
+    })
+
+    setTasks(tasksFiltered)
   }
 
   return (
